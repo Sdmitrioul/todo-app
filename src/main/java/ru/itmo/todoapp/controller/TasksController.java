b0044cc30af3066c98488b7c9b9cadd3e2e2973d
”Dmitri <dmitri.zd1958@gmail.com>
2022-10-22 15:11:46 +0200

    add default controller

--- src/main/java/ru/itmo/todoapp/controller/TasksController.java
+++ src/main/java/ru/itmo/todoapp/controller/TasksController.java
@@ -1,9 +1,14 @@
 package ru.itmo.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TasksController {
-
-
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String todoList(ModelMap map) {
+        return "index";
+    }
 }
